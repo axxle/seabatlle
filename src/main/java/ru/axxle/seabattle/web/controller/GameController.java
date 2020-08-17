960eb61ab71d3df1b2108b5c229a157cbb471fb5
axxle <kvv0911@gmail.com>
2020-08-17 15:35:59 +0300

    Temp test commit9

--- src/main/java/ru/axxle/seabattle/web/controller/GameController.java
+++ src/main/java/ru/axxle/seabattle/web/controller/GameController.java
@@ -4,7 +4,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import ru.axxle.seabattle.web.WebGame;
 
 
@@ -21,27 +24,31 @@ public class GameController {
 		return new ResponseEntity<WebGame>(newGame, HttpStatus.OK);
 	}
 
-	//ATTACH_TO_GAME	POST /game/{id:.+}/attach
 	@RequestMapping(value = BY_ID + "/attach", method = RequestMethod.GET)
-	public ResponseEntity<Object> attachToGame(@PathVariable String id) {
-		return new ResponseEntity<Object>("attachToGame(" + id + ")", HttpStatus.OK);
+	public ResponseEntity<WebGame> attachToGame(@PathVariable String id) {
+		WebGame game = new WebGame();
+		game.setName("attachToGame(" + id + ")");
+		return new ResponseEntity<WebGame>(game, HttpStatus.OK);
 	}
 
-	//MOVE				POST /game/{id:.+}/move
 	@RequestMapping(value = BY_ID + "/move", method = RequestMethod.GET)
-	public ResponseEntity<Object> move(@PathVariable String id) {
-		return new ResponseEntity<Object>("move(" + id + ")", HttpStatus.OK);
+	public ResponseEntity<WebGame> move(@PathVariable String id) {
+		WebGame game = new WebGame();
+		game.setName("move(" + id + ")");
+		return new ResponseEntity<WebGame>(game, HttpStatus.OK);
 	}
 
-	//GET_BY_ID			GET /game/{id:.+}
 	@RequestMapping(value = BY_ID, method = RequestMethod.GET)
-	public ResponseEntity<Object> getById(@PathVariable String id) {
-		return new ResponseEntity<Object>("getById(" + id + ");", HttpStatus.OK);
+	public ResponseEntity<WebGame> getById(@PathVariable String id) {
+		WebGame game = new WebGame();
+		game.setName("getById(" + id + ")");
+		return new ResponseEntity<WebGame>(game, HttpStatus.OK);
 	}
 
-	//LEAVE_GAME		POST /game/{id:.+}/leave
 	@RequestMapping(value = BY_ID + "/leave", method = RequestMethod.GET)
-	public ResponseEntity<Object> leaveGame(@PathVariable String id) {
-		return new ResponseEntity<Object>("leaveGame(" + id + ")", HttpStatus.OK);
+	public ResponseEntity<WebGame> leaveGame(@PathVariable String id) {
+		WebGame game = new WebGame();
+		game.setName("leaveGame(" + id + ")");
+		return new ResponseEntity<WebGame>(game, HttpStatus.OK);
 	}
 }
