12fcbcef4e07bad30adcea65ada95828e334ceb7
axxle <kvv0911@gmail.com>
2020-08-18 14:02:41 +0300

    Temp test commit17

--- src/main/java/ru/axxle/seabattle/web/WebGame.java
+++ src/main/java/ru/axxle/seabattle/web/WebGame.java
@@ -1,7 +1,17 @@
 package ru.axxle.seabattle.web;
 
 public class WebGame {
+
 	private String name;
+
+	private GameArea area1;
+
+	private GameArea area2;
+
+	public WebGame(){
+		this.area1 = new GameArea();
+		this.area2 = new GameArea();
+	}
 
 	public String getName() {
 		return name;
@@ -9,5 +19,21 @@ public class WebGame {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public GameArea getArea1() {
+		return area1;
+	}
+
+	public void setArea1(GameArea area1) {
+		this.area1 = area1;
+	}
+
+	public GameArea getArea2() {
+		return area2;
+	}
+
+	public void setArea2(GameArea area2) {
+		this.area2 = area2;
 	}
 }
